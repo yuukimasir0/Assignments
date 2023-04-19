4bc4dc75584ca599c03ed8ce50629111f0b066b8
Itsuki YOSHIDA <cmt14135@cs-d23.cs.nitech.ac.jp>
2023-04-19 11:59:15 +0900

    4/19

---
+++ N2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in `seq 21 25`
+do
+    ./compiler samples/ex$i.p
+    make asm
+    make sr
+    echo ""
+done
+
+for i in `seq 21 25`
+do
+    ./compiler samples/failure/ex${i}_fail.p
+done
